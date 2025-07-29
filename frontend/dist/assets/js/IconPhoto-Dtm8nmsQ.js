6a98b44c7e6079a27a39ed09e4d3b849a45a2743
nestor alvarez <nescool101@gmail.com>
2025-07-29 11:46:13 -0500

    fix: resolve Mantine dependency conflicts
    - Updated all @mantine packages to version 7.17.8
    - Resolved peer dependency conflicts
    - Build tested locally and successful
    - Ready for GitHub Pages deployment

---
+++ frontend/dist/assets/js/IconPhoto-Dtm8nmsQ.js
@@ -0,0 +1,11 @@
+import{c as a}from"./index-BGAR1JXy.js";/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var e=a("outline","file","IconFile",[["path",{d:"M14 3v4a1 1 0 0 0 1 1h4",key:"svg-0"}],["path",{d:"M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z",key:"svg-1"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var t=a("outline","photo","IconPhoto",[["path",{d:"M15 8h.01",key:"svg-0"}],["path",{d:"M3 6a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v12a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3v-12z",key:"svg-1"}],["path",{d:"M3 16l5 -5c.928 -.893 2.072 -.893 3 0l5 5",key:"svg-2"}],["path",{d:"M14 14l1 -1c.928 -.893 2.072 -.893 3 0l3 3",key:"svg-3"}]]);export{e as I,t as a};
